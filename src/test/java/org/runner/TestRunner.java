31c797eb5c70f4da7472df1d68a3bd9c3c721a7d
haripriya1781 <haripriya.elangovan@gmail.com>
2022-12-26 16:08:55 +0530

    test runner updated

--- src/test/java/org/runner/TestRunner.java
+++ src/test/java/org/runner/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src//test//resources", glue="org.stepdefinition",monochrome=true,dryRun=false)
+@CucumberOptions(features="src//test//resources", glue="org.stepdefinition",monochrome=true,dryRun=false,plugin={"json:target/cucumber/cumcumber.json"})
 public class TestRunner {
 
 }
